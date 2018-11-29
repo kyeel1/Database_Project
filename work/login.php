0354e6ffe532abc9010c2a5017b242012b4a2eae
kyeel1 <kyeel123@gmail.com>
2018-11-28 21:28:43 -0600

    Update login.php

--- work/login.php
+++ work/login.php
@@ -6,7 +6,7 @@
 <body>
 	<header>
 		<ul>
-		    <li><img src="logo.png"></li>
+		    <li><img src="images/logo.png"></li>
 			<li><a href="index.php"> HOME </a></li>
 			<li><a href="instructions.php"> INSTRUCTIONS </a></li>
 			<li><a href="The_Vault.php"> THE VAULT </a></li>
